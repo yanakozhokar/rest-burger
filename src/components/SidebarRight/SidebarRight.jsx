91797c52283778b70a88b183677b8a64e225a0a0
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-05-31 12:27:18 +0300

    feat: add sidebar-right markup and styles

--- src/components/SidebarRight/SidebarRight.jsx
+++ src/components/SidebarRight/SidebarRight.jsx
@@ -1,9 +1,29 @@
+import EmptyCart from '../../images/empty-cart.png';
 import './SidebarRight.css';
 
 export const SidebarRight = () => {
   return (
     <aside className="sidebar-right">
-      <p>My order</p>
+      <div className="sidebar-right__order">
+        <p className="sidebar-right__order-title">My order</p>
+        <div className="sidebar-right__order-container">
+          <div className="sidebar-right__empty-cart">
+            <div className="sidebar-right__empty-cart-img-container">
+              <img
+                src={EmptyCart}
+                alt="Empty cart"
+                width={174}
+                height={108}
+                className="sidebar-right__empty-cart-img"
+              />
+            </div>
+            <p className="sidebar-right__empty-cart-text">Cart empty</p>
+          </div>
+          <a href="/" className="sidebar-right__order-confirm">
+            Confirm order
+          </a>
+        </div>
+      </div>
     </aside>
   );
 };

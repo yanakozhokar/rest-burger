8e36c77133fc2deb90923af9e2ed654c0b395cb5
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-06-01 15:14:45 +0300

    update: add some styles to return-btn

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { IoMdReturnLeft } from 'react-icons/io';
 import { CartForm } from 'components/CartForm/CartForm';
 import { CartProductsTable } from 'components/CartProductsTable/CartProductsTable';
 import './Cart.css';
@@ -7,6 +8,7 @@ export const Cart = () => {
   return (
     <div className="cart container">
       <Link to="/" href="/" className="cart__return-btn">
+        <IoMdReturnLeft width={16} height={16} />
         Return
       </Link>
       <section className="cart__complete">

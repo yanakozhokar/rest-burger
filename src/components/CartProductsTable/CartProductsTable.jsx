7dd97903068b4bdacc8026bb1785b9ec0873f772
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-06-01 15:34:42 +0300

    feat: add possibility to delete some items from the order

--- src/components/CartProductsTable/CartProductsTable.jsx
+++ src/components/CartProductsTable/CartProductsTable.jsx
@@ -1,8 +1,11 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { IoMdTrash } from 'react-icons/io';
+import { deleteOrderItem } from 'redux/orderSlice';
 import './CartProductsTable.css';
 
 export const CartProductsTable = () => {
   const order = useSelector(state => state.order);
+  const dispatch = useDispatch();
 
   return (
     <table className="cart__complete-table">
@@ -11,6 +14,7 @@ export const CartProductsTable = () => {
           <th className="cart__complete-table-head-title">Product</th>
           <th className="cart__complete-table-head-title">Amount</th>
           <th className="cart__complete-table-head-title">Total</th>
+          <th className="cart__complete-table-head-title"></th>
         </tr>
       </thead>
       <tbody className="cart__complete-table-body">
@@ -19,6 +23,18 @@ export const CartProductsTable = () => {
             <td className="cart__complete-table-body-item">{item.name}</td>
             <td className="cart__complete-table-body-item">{item.amount}</td>
             <td className="cart__complete-table-body-item">{item.price}</td>
+            <td className="cart__complete-table-body-item">
+              <button
+                className="cart__complete-table-body-delete-btn"
+                onClick={() => dispatch(deleteOrderItem(item))}
+              >
+                <IoMdTrash
+                  width={18}
+                  height={18}
+                  className="cart__complete-table-body-trash-bin"
+                />
+              </button>
+            </td>
           </tr>
         ))}
       </tbody>

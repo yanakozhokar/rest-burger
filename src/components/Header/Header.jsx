9f69f5f3d6d15dd5501e3076617ec8dd3bd1bd2b
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-06-03 12:52:04 +0300

    update: add hover-effects to products items

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,11 @@ import './Header.css';
 export const Header = () => {
   return (
     <header className="header">
-      <div className="header__top">
-        <div className="container">
-          <Logo />
-          <a href="tel:+12122100020" className="header__tel">
-            +12122100020
-          </a>
-        </div>
+      <div className="container">
+        <Logo />
+        <a href="tel:+12122100020" className="header__tel">
+          +12122100020
+        </a>
       </div>
     </header>
   );
